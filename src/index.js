422a449e7cf8920e5542366610b7fbb5889e3644
V.Kharitonovitch <morainrallez@gmail.com>
2018-02-21 19:29:09 +0300

    first try

--- src/index.js
+++ src/index.js
@@ -1,30 +1,39 @@
 class Sorter {
   constructor() {
-    // your implementation
+    this.array = [];
   }
 
   add(element) {
-    // your implementation
+    this.array.push(element);
   }
 
   at(index) {
-    // your implementation
+   return this.array[index];
   }
 
   get length() {
-    // your implementation
+    return this.array.length;
   }
 
   toArray() {
-    // your implementation
+    return this.array;
   }
 
-  sort(indices) {
-    // your implementation
+  sort(indices) { //[7,6,5,48,93,1,0,14],index=[6,3,5,1];
+    let afterAllArray = this.array;
+    let preArray = []; //[6,48,1,0],after = [0,1,6,48]
+    let indicesAfterSort = indices.sort(); //[1,3,5,6]
+    for(let i = 0; i <= indices.length - 1; i++){
+      preArray.push(this.array[indices[i]]); 
+    }
+    for(let i = 0; i <= indices.length - 1; i++){
+      afterAllArray[indices[i]] = (preArray.sort())[i]; 
+    }
+    return afterAllArray; // [7,0,5,1,93,6,48,14]
   }
 
   setComparator(compareFunction) {
-    // your implementation
+    return this.array.sort(compareFunction);
   }
 }
 
